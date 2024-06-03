b518d8772f95721afea0accee0efe3b6fec39d3d
Arlene Joy Nacion <arlenejoy.nacion777@gmail.com>
2024-06-03 16:10:29 +0800

    fix: remove client info & correct the spelling

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -75,7 +75,7 @@ export const aboutData = [
     title: "experience",
     info: [
       {
-        title: "Techincal Support Specialist - Concentrix",
+        title: "Technical Support Specialist - Concentrix",
         stage: "2024",
       },
       {
@@ -151,15 +151,6 @@ const About = () => {
                 </div>
               </div>
 
-              {/* clients */}
-              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
-                <div className="text-2xl xl:text-4xl font-extrabold text-[#d580ff] mb-2">
-                  <CountUp start={0} end={10} duration={5} />
-                </div>
-                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
-                  Satisfied clients.
-                </div>
-              </div>
 
               {/* projects */}
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
